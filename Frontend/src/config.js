1463dead2328d14cd3244d80306bc63474226758
dua-zahra <duaezahra.b@gmail.com>
2026-06-23 03:29:22 +0300

    Hardcode API URL for production

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,3 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:65000';
+// const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:65000';
+const API_URL = 'https://studenova-backend.onrender.com';
 
 export default API_URL;
